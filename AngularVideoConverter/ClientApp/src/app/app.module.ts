c24e72719599c52460fc059f48d43f17b0ad55a6
moranmono <moranm22@gmail.com>
2020-05-16 13:13:47 +0300

    Adding file upload module

--- AngularVideoConverter/ClientApp/src/app/app.module.ts
+++ AngularVideoConverter/ClientApp/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileUploadComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ AngularVideoConverter/ClientApp/src/app/components/file-upload/file-upload.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpRequest, HttpEventType, HttpResponse } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-file-upload',
+  templateUrl: './file-upload.component.html',
+  styleUrls: ['./file-upload.component.css']
+})
+export class FileUploadComponent {
+  public progress: number;
+  public message: string;
+  constructor(private http: HttpClient) { }
+
+  fileUpload(files) {
+    if (files.length === 0) {
+      return;
+    }
+    const formData = new FormData();
+    for (let file of files) {
+      formData.append(file.name, file);
+    }
+    const uploadReq = new HttpRequest('POST', 'api/fileupload/file-upload', formData, {
+      reportProgress: true,
+    });
+
+    this.http.request(uploadReq).subscribe(event => {
+      if (event.type === HttpEventType.UploadProgress) {
+        this.progress = Math.round(100 * event.loaded / event.total);
+      }
+      else if (event.type === HttpEventType.Response) {
+        this.message = event.body.toString();
+      }
+    });
+  }
+
+}
