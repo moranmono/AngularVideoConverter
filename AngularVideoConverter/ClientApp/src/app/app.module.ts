9757cb74f6f7d801321daa5f73063088e3ee37bc
moranmono <moranm22@gmail.com>
2020-05-17 16:10:07 +0300

    Adding styles and nav bar

--- AngularVideoConverter/ClientApp/src/app/app.module.ts
+++ AngularVideoConverter/ClientApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FileUploadComponent } from './components/file-upload/file-upload.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FileUploadComponent
+    FileUploadComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
